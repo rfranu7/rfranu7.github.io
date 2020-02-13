15613c1589d16a8bd4b5af113ed1c35e5583dd36
Randeep Ranu <rfranu7@gmail.com>
2020-02-13 22:30:45 +0800

    commit lesson6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,7 @@
 //----------------- Wind Chill Calculator -----------------
 
-var tempC = parseFloat(document.getElementById("valHigh").textContent);
-var speed = parseFloat(document.getElementById("valWind").textContent);
+const tempC = parseFloat(document.getElementById("valHigh").textContent);
+const speed = parseFloat(document.getElementById("valWind").textContent);
 
 if(tempC < 11 && speed < 4){
 var tempF = (tempC * 9 /5) + 32; // Convert Celsius to Fahrenheit
@@ -9,11 +9,21 @@ var chill = windChill(tempF, speed); // Calculate Wind chill
 var chillC = (chill - 32) * 5/9; // Convert back to Celsius
 document.getElementById('valChill').textContent = chillC.toFixed(1);
 }
-
 else {
 	document.getElementById('valChill').textContent = "N/A";
 	document.getElementById('windUnit').textContent = "";
 }
+
+
+/* Fahrenheit calculator
+if (tempF < 51 && speed < 4){
+	var chill = windChill(tempF, speed);
+	document.getElementById('valChill').textContent = chill.toFixed(1); 
+}
+else {
+	document.getElementById('valChill').textContent = "N/A";
+	document.getElementById('windUnit').textContent = "";
+} */
 
 function windChill (TempF, speed) {
 	var speedPower = Math.pow(speed, 0.16);
