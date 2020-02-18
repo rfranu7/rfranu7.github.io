8283cee6ee0546c617e46ee85632165f83b31f35
Randeep Ranu <rfranu7@gmail.com>
2020-02-18 18:17:29 +0800

    commit lesson7

--- lesson7/js/lazyscript.js
+++ lesson7/js/lazyscript.js
@@ -1,16 +1,17 @@
 const images = document.querySelectorAll("[data-src]");
 
-function preloadImage(img){
+function preloadImage(img){ //substitutes the images
     const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
-
+    if (!src) { return; }
     img.src = src;
 }
 
+function removeImg(img){ //removes the data-src attribute so the blue effect in CSS will be removed
+    img.removeAttribute("data-src");
+}
+
 const ImgOptions = {
-    rootMargin: "0px 0px 100px 0px" 
+    rootMargin: "0px 0px -300px 0px" 
 };
 
 const ImgObserver = new IntersectionObserver((entries, ImgObserver) => {
@@ -20,6 +21,7 @@ const ImgObserver = new IntersectionObserver((entries, ImgObserver) => {
         } else {
             preloadImage(entry.target);
             ImgObserver.unobserve(entry.target);
+            removeImg(entry.target);
         }
     });
 }, ImgOptions);
@@ -27,3 +29,4 @@ const ImgObserver = new IntersectionObserver((entries, ImgObserver) => {
 images.forEach(image => {
     ImgObserver.observe(image);
 })
+
