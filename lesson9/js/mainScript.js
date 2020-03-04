6587f743613d7bbcd6f0000e50ad4771f7239a4b
Randeep Ranu <rfranu7@gmail.com>
2020-03-04 11:27:19 +0800

    commit lesson9

--- lesson9/js/mainScript.js
+++ lesson9/js/mainScript.js
@@ -16,6 +16,8 @@ fetch(requestURL)
         if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs"){       
 
             let card = document.createElement('section');
+            let div = document.createElement('div');
+            let div2 = document.createElement('div');
             let h2 = document.createElement('h2');
             let motto = document.createElement('strong');
             let yearFound = document.createElement('p');
@@ -23,20 +25,25 @@ fetch(requestURL)
             let rainfall = document.createElement('p');
             let image = document.createElement('img');
 
+            div.classList.add("col-1");
             h2.textContent = towns[i].name;
             motto.textContent = towns[i].motto;
             yearFound.textContent = "Year Founded: " + towns[i].yearFounded;
             population.textContent = "Population: " + towns[i].currentPopulation;
             rainfall.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+            div2.classList.add("col-2");
             image.setAttribute('src', "assets/" + towns[i].photo);
             image.setAttribute('alt', towns[i].name);
-
-            card.appendChild(h2);
-            card.appendChild(motto);
-            card.appendChild(yearFound);
-            card.appendChild(population);
-            card.appendChild(rainfall);
-            card.appendChild(image);
+            image.classList.add("altImg")
+            
+            card.appendChild(div);
+            card.appendChild(div2);
+            div.appendChild(h2);
+            div.appendChild(motto);
+            div.appendChild(yearFound);
+            div.appendChild(population);
+            div.appendChild(rainfall);
+            div2.appendChild(image);
 
             document.querySelector('div.cards').appendChild(card);
         }
