9ad6d528a0c1b8eeb010456ca058cf0e2b8655ef
Randeep Ranu <rfranu7@gmail.com>
2020-03-07 12:49:16 +0800

    commit lesson10

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=35b12c8d999fdda2699d5d2204b76ea4";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=35b12c8d999fdda2699d5d2204b76ea4";
 
 fetch(apiURL)
   .then((response) => response.json())
