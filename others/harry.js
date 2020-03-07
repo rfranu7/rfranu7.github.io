fba1ea95e5b94175810b82b6577bc4ccc3270851
Randeep Ranu <rfranu7@gmail.com>
2020-03-07 12:40:50 +0800

    commit lesson10

---
+++ others/harry.js
@@ -0,0 +1,6 @@
+const hambutton = document.getElementsByClassName('hamburger')[0];
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+	document.getElementsByClassName("navBar")[0].classList.toggle("responsive");
+};
