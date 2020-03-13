aa820803d43409510354bde7242bab45d4a996cd
Randeep Ranu <rfranu7@gmail.com>
2020-03-13 12:52:18 +0800

    commit lesson11

--- lesson11/js/townScript.js
+++ lesson11/js/townScript.js
@@ -51,6 +51,27 @@ if(today == 5) {
 	document.getElementById("popup").className = "show";
 }
 
+//----------------- Wind Chill Calculator -----------------
+
+const tempF = parseFloat(document.getElementById("valHigh").textContent);
+const speed = parseFloat(document.getElementById("valWind").textContent);
+
+if (tempF < 51 && speed < 4){
+	var chill = windChill(tempF, speed);
+	document.getElementById('valChill').textContent = chill.toFixed(1); 
+}
+else {
+	document.getElementById('valChill').textContent = "N/A";
+	document.getElementById('windUnit').textContent = "";
+}
+
+function windChill (TempF, speed) {
+	var speedPower = Math.pow(speed, 0.16);
+	var windChillFactor = 35.74 + (0.6215 * TempF) - (35.75 * speedPower) + (0.4275 * TempF * speedPower);
+	return windChillFactor;
+}
+
+
 
 //-----------------Weather Data------------------
 
