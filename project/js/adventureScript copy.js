64d6fe05db4cae7c1ab8b40accab247b981ebe0f
Randeep Ranu <rfranu7@gmail.com>
2020-03-28 12:17:53 +0800

    commit updates

--- project/js/adventureScript copy.js
+++ project/js/adventureScript copy.js
@@ -7,6 +7,59 @@ function toggleMenu() {
 	document.getElementsByClassName("navBar")[0].classList.toggle("responsive");
 };
 
+//-----------------Trips Data---------------------
+
+const requestURL = 'https://rfranu7.github.io/project/objects/trips.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const trip = jsonObject['trips'];
+
+    for (let i = 0; i < trip.length; i++ ) {
+
+            let div = document.createElement('div');
+
+            let divImg = document.createElement('div');
+            let image = document.createElement('img');
+
+            let div2 = document.createElement('div');
+            let h3 = document.createElement('h3');
+            let desc = document.createElement('p');
+            let link = document.createElement('a');
+            let btn = document.createElement('button');
+
+            image.setAttribute("src", "assets/" + trip[i].photo);
+            image.setAttribute("alt", trip[i].name);
+
+            h3.textContent = trip[i].name;
+            desc.textContent = trip[i].desc;
+            btn.textContent = "Explore";
+
+
+            div.appendChild(divImg);
+            div.appendChild(div2);
+            div.classList.add("trip-data");
+
+            divImg.appendChild(image);
+
+            div2.appendChild(h3);
+            div2.appendChild(desc);
+            div2.appendChild(link);
+
+            link.appendChild(btn);
+            btn.classList.add("book-btn");
+            link.setAttribute("href", "adventures-trip-number" +[i+1]+ ".html")
+            
+            document.querySelector('section.trips').appendChild(div);
+        }
+    }
+  );
+
 //-----------------Lazy Loading------------------
 
 const images = document.querySelectorAll("[data-src], [data-srcset]");
