ee2242d42ebd0cd72b4fb61cf080cdf5996f7039
Randeep Ranu <rfranu7@gmail.com>
2020-04-02 17:08:56 +0800

    blog update

--- project/js/contactScript.js
+++ project/js/contactScript.js
@@ -6,6 +6,46 @@ hambutton.addEventListener("click", toggleMenu, false);
 function toggleMenu() {
 	document.getElementsByClassName("navBar")[0].classList.toggle("responsive");
 };
+
+//-----------------Lazy Loading------------------
+
+const images = document.querySelectorAll("[data-src], [data-srcset]");
+
+function preloadImage(img){ //substitutes the images
+    const src = img.getAttribute("data-src");
+    const srcset = img.getAttribute("data-srcset");
+    if (!src) { 
+        if (!srcset) { return; }
+        else {img.srcset = srcset;}
+    }
+    else { img.src = src;}
+}
+
+function removeImg(img){ //removes the data-src attribute so the blue effect in CSS will be removed
+    img.removeAttribute("data-src");
+    img.removeAttribute("data-srcset");
+}
+
+const ImgOptions = {
+    rootMargin: "0px 0px -300px 0px" 
+};
+
+const ImgObserver = new IntersectionObserver((entries, ImgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            ImgObserver.unobserve(entry.target);
+            removeImg(entry.target);
+        }
+    });
+}, ImgOptions);
+
+images.forEach(image => {
+    ImgObserver.observe(image);
+})
+
   
 //-----------------Footer Script-----------------
 
