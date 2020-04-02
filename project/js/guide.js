24620cda55f379573f2acceb7d408ba7e4e4b94e
Randeep Ranu <rfranu7@gmail.com>
2020-04-02 10:25:05 +0800

    commit project css creation

--- project/js/guide.js
+++ project/js/guide.js
@@ -7,45 +7,6 @@ function toggleMenu() {
 	document.getElementsByClassName("navBar")[0].classList.toggle("responsive");
 };
 
-//-----------------Lazy Loading------------------
-
-const images = document.querySelectorAll("[data-src], [data-srcset]");
-
-function preloadImage(img){ //substitutes the images
-    const src = img.getAttribute("data-src");
-    const srcset = img.getAttribute("data-srcset");
-    if (!src) { 
-        if (!srcset) { return; }
-        img.srcset = srcset;
-    }
-    img.src = src;
-}
-
-function removeImg(img){ //removes the data-src attribute so the blue effect in CSS will be removed
-    img.removeAttribute("data-src");
-    img.removeAttribute("data-srcset");
-}
-
-const ImgOptions = {
-    rootMargin: "0px 0px -300px 0px" 
-};
-
-const ImgObserver = new IntersectionObserver((entries, ImgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            ImgObserver.unobserve(entry.target);
-            removeImg(entry.target);
-        }
-    });
-}, ImgOptions);
-
-images.forEach(image => {
-    ImgObserver.observe(image);
-})
-
 //-----------------Trips Data---------------------
 
   const requestURL = 'https://rfranu7.github.io/project/objects/guides.json';
