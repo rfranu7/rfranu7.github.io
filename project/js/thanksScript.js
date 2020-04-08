fb907b5f23dc1d1923d12408674fdae7a7ad03a4
Randeep Ranu <rfranu7@gmail.com>
2020-04-08 23:57:24 +0800

    final push

---
+++ project/js/thanksScript.js
@@ -0,0 +1,18 @@
+//----------------- Responsive Menu -----------------
+
+const hambutton = document.getElementsByClassName('hamburger')[0];
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+	document.getElementsByClassName("navBar")[0].classList.toggle("responsive");
+};
+  
+//-----------------Footer Script-----------------
+
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+let date = new Date();
+let year = date.getFullYear();
+
+document.getElementById("currentYear").textContent = year;
